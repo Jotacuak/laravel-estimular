8c1bca08f121bbf2e36499fed4253692996d5c8d
jotacuak <jaimejsastre@gmail.com>
2022-04-24 20:00:22 +0200

    pre-carrousel

--- resources/js/front/app-mobile.js
+++ resources/js/front/app-mobile.js
@@ -1,6 +1,8 @@
 import {renderMenu} from './mobile/menu.js';
 // import './desktop/header.js'
-import { renderForm } from './desktop/renderForm.js';
+import { renderForm } from './mobile/renderForm.js';
+import { renderCarrousel } from './mobile/carrousel.js'
 
 renderForm ();
 renderMenu();
+renderCarrousel();
---
+++ resources/views/front/components/mobile/main_prices.blade.php
@@ -0,0 +1,153 @@
+<div class="price-cards">
+    <div class="price-container">
+
+        <div class="desktop-one-column">
+            <div class="column">
+                <div class="swich-button">
+                    <label class="switch xl">
+                        <input id="swich-btn" type="checkbox">
+                        <span class="slider-bar round"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    
+        <div class="cards-elements" id="card-elements">
+            <div class="wrapper">
+                <div class="items" id="items">
+    
+                    <div class="card-container">
+                        <div class="card-element side-a ">
+                            <div class="card-element-image">
+                                <img src="{{Storage::url('prices-card.webp')}}" alt="alquiler de llaut en Mallorca">
+                            </div>    
+                            <div  class="card-element-description">
+                                <div class="card-element-price">
+                                    <h2>€40/día</h2>
+                                </div>
+                                <div class="card-element-title">
+                                    <h3>Una cita a la semana</h3>
+                                </div>
+                                <div class="card-element-text">
+                                    <p> Meis vocent signiferumque pri et. Facilis corpora recusabo ne quo</p>
+                                </div>
+                                <div class="card-element-button">
+                                    <button type="button" class="price-button">Contáctanos</button>
+                                </div>
+                            </div>
+                        </div>
+    
+                        <div class="card-element side-b">
+                            <div class="card-element-image">
+                                <img src="{{Storage::url('prices-card.webp')}}" alt="alquiler de llaut en Mallorca">
+                            </div>    
+                            <div  class="card-element-description">
+                                <div class="card-element-price">
+                                    <h2>€45/día</h2>
+                                </div>
+                                <div class="card-element-title">
+                                    <h3>Una cita a la semana</h3>
+                                </div>
+                                <div class="card-element-text">
+                                    <p> Meis vocent signiferumque pri et. Facilis corpora recusabo ne quo</p>
+                                </div>
+                                <div class="card-element-button">
+                                    <button type="button" class="price-button">Contáctanos</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="card-container">
+                        <div class="card-element side-a">
+                            <div class="card-element-image">
+                                <img src="{{Storage::url('prices-card.webp')}}" alt="alquiler de llaut en Mallorca">
+                            </div>      
+                            <div  class="card-element-description">
+                                <div class="card-element-price">
+                                    <h2>€140</h2>
+                                </div>
+                                <div class="card-element-title">
+                                    <h3>Paquete de 4 sesiones</h3>
+                                </div>
+                                <div class="card-element-text">
+                                    <p> Meis vocent signiferumque pri et. Facilis corpora recusabo ne quo</p>
+                                </div>
+                                <div class="card-element-button">
+                                    <button type="button" class="price-button">Contáctanos</button>
+                                </div>
+                            </div>
+                        </div>
+    
+                        <div class="card-element side-b">
+                            <div class="card-element-image">
+                                <img src="{{Storage::url('prices-card.webp')}}" alt="alquiler de llaut en Mallorca">
+                            </div>   
+                            <div  class="card-element-description">
+                                <div class="card-element-price">
+                                    <h2>€150</h2>
+                                </div>
+                                <div class="card-element-title">
+                                    <h3>Paquete de 4 sesiones</h3>
+                                </div>
+                                <div class="card-element-text">
+                                    <p> Meis vocent signiferumque pri et. Facilis corpora recusabo ne quo</p>
+                                </div>
+                                <div class="card-element-button">
+                                    <button type="button" class="price-button">Contáctanos</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="card-container">
+                        <div class="card-element side-a">
+                            <div class="card-element-image">
+                                <img src="{{Storage::url('prices-card.webp')}}" alt="alquiler de llaut en Mallorca">
+                            </div>     
+                            <div  class="card-element-description">
+                                <div class="card-element-price">
+                                    <h2>€260</h2>
+                                </div>
+                                <div class="card-element-title">
+                                    <h3>Paquete de 8 sesiones</h3>
+                                </div>
+                                <div class="card-element-text">
+                                    <p> Meis vocent signiferumque pri et. Facilis corpora recusabo ne quo</p>
+                                </div>
+                                <div class="card-element-button">
+                                    <button type="button" class="price-button">Contáctanos</button>
+                                </div>
+                            </div>
+                        </div>
+    
+                        <div class="card-element side-b">
+                            <div class="card-element-image">
+                                <img src="{{Storage::url('prices-card.webp')}}" alt="alquiler de llaut en Mallorca">
+                            </div>
+                            <div  class="card-element-description">
+                                <div class="card-element-price">
+                                    <h2>€285</h2>
+                                </div>
+                                <div class="card-element-title">
+                                    <h3>Paquete de 8 sesiones</h3>
+                                </div>
+                                <div class="card-element-text">
+                                    <p> Meis vocent signiferumque pri et. Facilis corpora recusabo ne quo</p>
+                                </div>
+                                <div class="card-element-button">
+                                    <button type="button" class="price-button">Contáctanos</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>   
+    
+                </div>
+            </div>
+
+            <a id="prev" class="control prev"></a>
+            <a id="next" class="control next"></a>
+            
+        </div>
+    </div>   
+</div>
