93930a116a36ce6bc5a895b2b4626886015552da
Jotacuak <jaimejsastre@gmail.com>
2022-05-09 19:02:44 +0200

    JS cambio nombres

--- resources/js/front/app-mobile.js
+++ resources/js/front/app-mobile.js
@@ -1,8 +1,12 @@
 import {renderMenu} from './mobile/menu.js';
 // import './desktop/header.js'
 import { renderForm } from './mobile/renderForm.js';
-import { renderCarrousel } from './mobile/carrousel.js'
+import { renderCarrousel } from './mobile/carrousel.js';
+import { renderAccordion } from './mobile/accordion.js';
+import { renderCard } from './desktop/cardFlip.js';
 
 renderForm ();
 renderMenu();
 renderCarrousel();
+renderAccordion();
+renderCard();
--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -1,15 +1,16 @@
 require('../bootstrap');
 
 import { renderForm } from './desktop/renderForm.js';
-import { flipCard } from './desktop/cardFlip.js';
-import { accordionRender } from './desktop/accordion.js';
+import { renderCard } from './desktop/cardFlip.js';
+import { renderAccordion } from './desktop/accordion.js';
 import { renderMenu } from './desktop/menu.js';
 
 // import { memoryGame } from './desktop/renderMemory.js';
 
 
 renderForm ();
-flipCard ();
-accordionRender ();
+renderCard ();
+renderAccordion ();
 renderMenu();
+
 // memoryGame ();
